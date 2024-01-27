3c43b692fb05e7bc14350a10f2807089d2874971
Charles Severance <csev@umich.edu>
2024-01-27 12:05:43 -0500

    Updates.

--- djlmp/admin.py
+++ djlmp/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 
-from .models import Tenant
+from .models import Tenant, Context, Subject, Link, LineItem, Membership, Score, ContextLog
 
 admin.site.register(Tenant)
+admin.site.register(Context)
+admin.site.register(Subject)
+admin.site.register(Link)
+admin.site.register(LineItem)
+admin.site.register(Membership)
+admin.site.register(Score)
+admin.site.register(ContextLog)
 
 # Register your models here.
