951b6b63eed51b2e57331b6527dea730a6711280
Charles Severance <csev@umich.edu>
2024-01-28 11:28:08 -0500

    Updates.

--- djlmp/apps.py
+++ djlmp/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class DjlmpConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'djlmp'
+
+    # This causes a warning: avoid executing queries in AppConfig.ready()
+    # Later we will find a better way to fix this
+    def ready(self) :
+        from .models import ContextRole
+        for role in ContextRole.CONTEXT_ROLE_CHOICES:
+            c, created = ContextRole.objects.get_or_create(context_role=role)
+            if created : print(role)
